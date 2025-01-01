c556d785ebe5e2754466d94ab0c721ee6a29f8be
ShougotMollik <shougotmollik@gmail.com>
2025-01-02 00:33:35 +0600

    - design movies details screen

---
+++ lib/utils/constants/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color whiteColor = Colors.white;
+  static Color blackColor = Colors.black;
+  static Color redColor = Colors.red;
+}
